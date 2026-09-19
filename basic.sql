20ac290923c7e9c1cb0c89d4f3a1e99e34a44b16
Anh Duc Tran <kieranlucas.work@gmail.com>
2026-09-20 00:32:38 +0700

    Update basic.sql

--- basic.sql
+++ basic.sql
@@ -23,3 +23,20 @@ UPDATE students SET age = 20 WHERE id = 2;
 SELECT * FROM students;
 INSERT INTO students VALUE (99, 'Test', 99);
 DELETE FROM students WHERE id = 99;
+SELECT * FROM students 
+WHERE age >= 20 AND id != 2;
+SELECT * FROM students
+ORDER BY age DESC
+LIMIT 2;
+SELECT * FROM students 
+WHERE age IS NOT NULL;
+CREATE DATABASE class;
+USE class;
+CREATE TABLE student_groups1 (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    groups_id INT,
+    FOREIGN KEY (groups_id) REFERENCES students(id)
+);
+
+
